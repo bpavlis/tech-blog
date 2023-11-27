74cf679d11a1610d9799743dfffbfea93c76bc2c
bpavlis <benpavlis@gmail.com>
2023-11-26 19:12:58 -0600

    last of the minor server change

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
-const app = express();
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require("path");
 
 const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const path = require("path");
-
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const sess = {
   secret: 'gary doesnt know',
@@ -24,24 +20,24 @@ const sess = {
   }),
 };
 
-app.use(session(sess));
-
+const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-//middleware
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
-const okToSync = (process.env.NODE_ENV === "production") ? false : false;
-sequelize.sync({ force: okToSync }).then(() => {
-  app.listen(PORT, () => console.log(`now listening at http://localhost:${PORT}`));
-});
-// app.listen(PORT, () =>
-//   console.log(`App listening at http://localhost:${PORT}`)
-// );
+// const okToSync = (process.env.NODE_ENV === "production") ? false : true;
+// sequelize.sync({ force: okToSync }).then(() => {
+//   app.listen(PORT, () => console.log(`now listening at http://localhost:${PORT}`));
+// });
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
